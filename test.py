ad0321e4ae188ee9d4bc0944cc38d911fa9c7b50
semitrivial <alexander@math.ohio-state.edu>
2019-12-11 16:05:54 -0500

    Override subtraction in test.py

--- test.py
+++ test.py
@@ -20,6 +20,11 @@ class Notation:
         assert output_was_called
         assert output_counter == output_limit
         return captured_output
+    def __sub__(self, subtractand):
+        if subtractand == 0:
+            return self
+        else:
+            return get_sole_output(self) - (subtractand-1)
 
 output_counter = -1
 output_was_called = False
@@ -58,7 +63,7 @@ def get_file(filename):
 def ord_file(filename):
     return Notation(get_file(filename))
 
-def count_outputs(x, upper_limit=99):
+def count_outputs(x, upper_limit=9):
     global output_counter
     global output_limit
 
@@ -75,86 +80,70 @@ def get_sole_output(x):
     assert count_outputs(x) == 1
     return x[0]
 
-def test_after_decrements(x, test_fnc, num_decrements):
-    if num_decrements == 0:
-        return test_fnc(x)
-    else:
-        return test_after_decrements(get_sole_output(x), test_fnc, num_decrements - 1)
-
-def looks_like_zero(x):
+def is_zero(x):
     return count_outputs(x) == 0
 
 print("Testing 0.py")
-assert looks_like_zero(ord_file("0.py"))
+assert is_zero(ord_file("0.py"))
 
 print("Testing 1.py, 2.py, 3.py")
-assert test_after_decrements(ord_file("1.py"), looks_like_zero, 1)
-assert test_after_decrements(ord_file("2.py"), looks_like_zero, 2)
-assert test_after_decrements(ord_file("3.py"), looks_like_zero, 3)
+assert is_zero(ord_file("1.py")-1)
+assert is_zero(ord_file("2.py")-2)
+assert is_zero(ord_file("3.py")-3)
 
 def looks_like_omega(x):
-    i = 0
-    while i < 5:
-        y = x[i]
-        if not test_after_decrements(y, looks_like_zero, i):
-            return False
-        i += 1
+    assert is_zero(x[0])
+    assert is_zero(x[1]-1)
+    assert is_zero(x[2]-2)
+    assert is_zero(x[3]-3)
+    assert is_zero(x[4]-4)
     return True
 
 print("Testing w.py")
 assert looks_like_omega(ord_file("w.py"))
 
 print("Testing w+1.py, w+2.py, w+3.py")
-assert test_after_decrements(ord_file("w+1.py"), looks_like_omega, 1)
-assert test_after_decrements(ord_file("w+2.py"), looks_like_omega, 2)
-assert test_after_decrements(ord_file("w+3.py"), looks_like_omega, 3)
-
-def looks_like_omega_times2(x):
-    i = 0
-    while i < 5:
-        y = x[i]
-        if not test_after_decrements(y, looks_like_omega, i):
-            return False
-        i += 1
-    return True
+assert looks_like_omega(ord_file("w+1.py")-1)
+assert looks_like_omega(ord_file("w+2.py")-2)
+assert looks_like_omega(ord_file("w+3.py")-3)
 
 print("Testing w*2.py, w*2+1.py")
-assert looks_like_omega_times2(ord_file("w*2.py"))
-assert test_after_decrements(ord_file("w*2+1.py"), looks_like_omega_times2, 1)
-
-def looks_like_omega_times3(x):
-    i = 0
-    while i < 5:
-        y = x[i]
-        if not test_after_decrements(y, looks_like_omega_times2, i):
-            return False
-        i += 1
+def looks_like_omega_times2(x):
+    assert looks_like_omega(x[0])
+    assert looks_like_omega(x[1]-1)
+    assert looks_like_omega(x[2]-2)
     return True
-
+assert looks_like_omega_times2(ord_file("w*2.py"))
+assert looks_like_omega_times2(ord_file("w*2.py")-1)
 
 print("Testing w*3.py")
+def looks_like_omega_times3(x):
+    assert looks_like_omega_times2(x[0])
+    assert looks_like_omega_times2(x[1]-1)
+    assert looks_like_omega_times2(x[2]-2)
+    return True
 assert looks_like_omega_times3(ord_file("w*3.py"))
 
+print("Testing w^2.py")
 def looks_like_omegasquared(x):
     assert looks_like_omega(x[0])
     assert looks_like_omega_times2(x[1])
     assert looks_like_omega_times3(x[2])
     return True
-
-print("Testing w^2.py")
 assert looks_like_omegasquared(ord_file("w^2.py"))
 
-def looks_like_omegasquared_plus_omega(x):
-    assert test_after_decrements(x[0], looks_like_omegasquared, 0)
-    assert test_after_decrements(x[1], looks_like_omegasquared, 1)
-    assert test_after_decrements(x[2], looks_like_omegasquared, 2)
-    return True
-
 print("Testing w^2+w.py")
+def looks_like_omegasquared_plus_omega(x):
+    assert looks_like_omegasquared(x[0])
+    assert looks_like_omegasquared(x[1]-1)
+    assert looks_like_omegasquared(x[2]-2)
+    return True
 assert looks_like_omegasquared_plus_omega(ord_file("w^2+w.py"))
 
 print("Testing w^2+w*2.py")
-assert looks_like_omegasquared_plus_omega(ord_file("w^2+w*2.py")[0])
-assert test_after_decrements(ord_file("w^2+w*2.py")[1], looks_like_omegasquared_plus_omega, 1)
-assert test_after_decrements(ord_file("w^2+w*2.py")[2], looks_like_omegasquared_plus_omega, 2)
-assert test_after_decrements(ord_file("w^2+w*2.py")[3], looks_like_omegasquared_plus_omega, 3)
+def looks_like_omegasquared_plus_omegatimes2(x):
+    assert looks_like_omegasquared_plus_omega(x[0])
+    assert looks_like_omegasquared_plus_omega(x[1]-1)
+    assert looks_like_omegasquared_plus_omega(x[2]-2)
+    return True
+assert looks_like_omegasquared_plus_omegatimes2(ord_file("w^2+w*2.py"))
