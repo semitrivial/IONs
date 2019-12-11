d7b973cd5364491256058681308fc6a54775bc80
semitrivial <alexander@math.ohio-state.edu>
2019-12-10 21:28:07 -0500

    Oops, forgot to commit w^2+w.py

---
+++ w^2+w.py
@@ -0,0 +1,22 @@
+X="""
+LEFT=\"\"\"
+X=\\\"\\\"\\\"
+\"\"\"
+
+RIGHT=\"\"\"
+\\\"\\\"\\\"
+while True:
+    output(X)
+    X = \\\"output(\\\\\\\"\\\\\\\"\\\\\\\"\" + escape(X) + \"\\\\\\\"\\\\\\\"\\\\\\\")\\\"
+\"\"\"
+
+X=\"\"
+
+while True:
+    X = LEFT + escape(X) + RIGHT
+    output(X)
+"""
+
+while True:
+    output(X)
+    X = "output(\"\"\"\n" + escape(X) + "\n\"\"\")"
