c3d7190f66baff53a022d6e9ad0a7b2d50cac120
semitrivial <alexander@math.ohio-state.edu>
2019-12-11 08:34:55 -0500

    Introduce Notation class

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
 # _*_ coding: utf _*_
 
+class Notation:
+    def __init__(self, txt):
+        self.txt = txt
+    def __getitem__(self, key):
+        return get_nth_output(self, key)
+
 output_counter = -1
 output_was_called = False
 output_limit = -1
@@ -7,12 +13,12 @@ captured_output = None
 
 def try_exec(x):
     try:
-        exec(x)
+        exec(x.txt)
     except StopIteration:
         raise
     except Exception as e:
         print("---")
-        print(x)
+        print(x.txt)
         print("---")
         raise e
 
@@ -23,7 +29,7 @@ def output(x):
 
     output_was_called = True
     output_counter += 1
-    captured_output = x.strip()
+    captured_output = Notation(x)
     if output_counter == output_limit:
         raise StopIteration("Done")
 
@@ -32,9 +38,12 @@ def escape(s):
 
 def get_file(filename):
     with open(filename, "r") as fp:
-        return fp.read().strip()
+        return fp.read()
 
-def get_nth_output(str, n):
+def ord_file(filename):
+    return Notation(get_file(filename))
+
+def get_nth_output(x, n):
     global output_counter
     global output_limit
     global output_was_called
@@ -44,7 +53,7 @@ def get_nth_output(str, n):
     output_was_called = False
 
     try:
-        try_exec(str)
+        try_exec(x)
     except StopIteration:
         pass
 
@@ -52,103 +61,103 @@ def get_nth_output(str, n):
     assert output_counter == output_limit
     return captured_output
 
-def count_outputs(str, upper_limit=99):
+def count_outputs(x, upper_limit=99):
     global output_counter
     global output_limit
 
     output_limit = upper_limit
     output_counter = -1
-    try_exec(str)
+    try_exec(x)
 
     if output_counter == upper_limit:
         raise ValueError("Count_outputs called on an apparently infinite sequence")
 
     return output_counter + 1
 
-def get_sole_output(str):
-    assert count_outputs(str) == 1
-    return get_nth_output(str, 0)
+def get_sole_output(x):
+    assert count_outputs(x) == 1
+    return get_nth_output(x, 0)
 
-def test_after_decrements(str, test_fnc, num_decrements):
+def test_after_decrements(x, test_fnc, num_decrements):
     if num_decrements == 0:
-        return test_fnc(str)
+        return test_fnc(x)
     else:
-        return test_after_decrements(get_sole_output(str), test_fnc, num_decrements - 1)
+        return test_after_decrements(get_sole_output(x), test_fnc, num_decrements - 1)
 
-def looks_like_zero(str):
-    return count_outputs(str) == 0
+def looks_like_zero(x):
+    return count_outputs(x) == 0
 
 print("Testing 0.py")
-assert looks_like_zero(get_file("0.py"))
+assert looks_like_zero(ord_file("0.py"))
 
 print("Testing 1.py, 2.py, 3.py")
-assert test_after_decrements(get_file("1.py"), looks_like_zero, 1)
-assert test_after_decrements(get_file("2.py"), looks_like_zero, 2)
-assert test_after_decrements(get_file("3.py"), looks_like_zero, 3)
+assert test_after_decrements(ord_file("1.py"), looks_like_zero, 1)
+assert test_after_decrements(ord_file("2.py"), looks_like_zero, 2)
+assert test_after_decrements(ord_file("3.py"), looks_like_zero, 3)
 
-def looks_like_omega(str):
+def looks_like_omega(x):
     i = 0
     while i < 5:
-        x = get_nth_output(str, i)
-        if not test_after_decrements(x, looks_like_zero, i):
+        y = get_nth_output(x, i)
+        if not test_after_decrements(y, looks_like_zero, i):
             return False
         i += 1
     return True
 
 print("Testing w.py")
-assert looks_like_omega(get_file("w.py"))
+assert looks_like_omega(ord_file("w.py"))
 
 print("Testing w+1.py, w+2.py, w+3.py")
-assert test_after_decrements(get_file("w+1.py"), looks_like_omega, 1)
-assert test_after_decrements(get_file("w+2.py"), looks_like_omega, 2)
-assert test_after_decrements(get_file("w+3.py"), looks_like_omega, 3)
+assert test_after_decrements(ord_file("w+1.py"), looks_like_omega, 1)
+assert test_after_decrements(ord_file("w+2.py"), looks_like_omega, 2)
+assert test_after_decrements(ord_file("w+3.py"), looks_like_omega, 3)
 
-def looks_like_omega_times2(str):
+def looks_like_omega_times2(x):
     i = 0
     while i < 5:
-        x = get_nth_output(str, i)
-        if not test_after_decrements(x, looks_like_omega, i):
+        y = get_nth_output(x, i)
+        if not test_after_decrements(y, looks_like_omega, i):
             return False
         i += 1
     return True
 
 print("Testing w*2.py, w*2+1.py")
-assert looks_like_omega_times2(get_file("w*2.py"))
-assert test_after_decrements(get_file("w*2+1.py"), looks_like_omega_times2, 1)
+assert looks_like_omega_times2(ord_file("w*2.py"))
+assert test_after_decrements(ord_file("w*2+1.py"), looks_like_omega_times2, 1)
 
-def looks_like_omega_times3(str):
+def looks_like_omega_times3(x):
     i = 0
     while i < 5:
-        x = get_nth_output(str, i)
-        if not test_after_decrements(x, looks_like_omega_times2, i):
+        y = get_nth_output(x, i)
+        if not test_after_decrements(y, looks_like_omega_times2, i):
             return False
         i += 1
     return True
 
 
 print("Testing w*3.py")
-assert looks_like_omega_times3(get_file("w*3.py"))
+assert looks_like_omega_times3(ord_file("w*3.py"))
 
-def looks_like_omegasquared(str):
-    assert looks_like_omega(get_nth_output(str, 0))
-    assert looks_like_omega_times2(get_nth_output(str, 1))
-    assert looks_like_omega_times3(get_nth_output(str, 2))
+def looks_like_omegasquared(x):
+    assert looks_like_omega(get_nth_output(x, 0))
+    assert looks_like_omega_times2(get_nth_output(x, 1))
+    assert looks_like_omega_times3(get_nth_output(x, 2))
     return True
 
 print("Testing w^2.py")
-assert looks_like_omegasquared(get_file("w^2.py"))
+assert looks_like_omegasquared(ord_file("w^2.py"))
 
-def looks_like_omegasquared_plus_omega(str):
-    assert test_after_decrements(get_nth_output(str, 0), looks_like_omegasquared, 0)
-    assert test_after_decrements(get_nth_output(str, 1), looks_like_omegasquared, 1)
-    assert test_after_decrements(get_nth_output(str, 2), looks_like_omegasquared, 2)
+def looks_like_omegasquared_plus_omega(x):
+    assert test_after_decrements(get_nth_output(x, 0), looks_like_omegasquared, 0)
+    assert test_after_decrements(get_nth_output(x, 1), looks_like_omegasquared, 1)
+    assert test_after_decrements(get_nth_output(x, 2), looks_like_omegasquared, 2)
     return True
 
 print("Testing w^2+w.py")
-assert looks_like_omegasquared_plus_omega(get_file("w^2+w.py"))
+assert looks_like_omegasquared_plus_omega(ord_file("w^2+w.py"))
 
 print("Testing w^2+w*2.py")
-assert looks_like_omegasquared_plus_omega(get_nth_output(get_file("w^2+w*2.py"), 0))
-assert test_after_decrements(get_nth_output(get_file("w^2+w*2.py"), 1), looks_like_omegasquared_plus_omega, 1)
-assert test_after_decrements(get_nth_output(get_file("w^2+w*2.py"), 2), looks_like_omegasquared_plus_omega, 2)
-assert test_after_decrements(get_nth_output(get_file("w^2+w*2.py"), 3), looks_like_omegasquared_plus_omega, 3)
+assert looks_like_omegasquared_plus_omega(get_nth_output(ord_file("w^2+w*2.py"), 0))
+assert test_after_decrements(get_nth_output(ord_file("w^2+w*2.py"), 1), looks_like_omegasquared_plus_omega, 1)
+assert test_after_decrements(get_nth_output(ord_file("w^2+w*2.py"), 2), looks_like_omegasquared_plus_omega, 2)
+assert test_after_decrements(get_nth_output(ord_file("w^2+w*2.py"), 3), looks_like_omegasquared_plus_omega, 3)
