7f05d6337f21a857114ca0bf7f44f4548533e15e
semitrivial <alexander@math.ohio-state.edu>
2019-12-11 22:32:06 -0500

    Compactify w^3.py

--- w^3.py
+++ w^3.py
@@ -1,14 +1,11 @@
 TEMPLATE='''
 TEMPLATE="""
 X=\'''{}\'''
-
 while True:
     output(X)
     X='output(\\\"""' + escape(X) + '\\\""")'
 """
-
 X=\'''{}\'''
-
 while True:
     output(X)
     X = TEMPLATE.format(escape(X))
