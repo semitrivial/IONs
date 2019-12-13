42804b9b33586b26b54a0db69adc6b0ba388ac47
semitrivial <alexander@math.ohio-state.edu>
2019-12-13 17:15:57 -0500

    Add a draft of what might be w^w*2

--- src/Lv6/test.py
+++ src/Lv6/test.py
@@ -3,3 +3,11 @@ from util import *
 def test_level_6():
     print("Testing w^w.py")
     assert looks_like_omega_to_omega(ord_file("w^w"))
+
+    print("Testing w^w*2.py")
+    def looks_like_wtow_times2(x):
+        import pdb; pdb.set_trace()
+        assert looks_like_omega_to_omega(x[0])
+        assert resembles(x[1], add_omega(x[0]))
+        return True
+    assert looks_like_wtow_times2(ord_file("w^w*2"))
