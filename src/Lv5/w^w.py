c4879ac90b8f3b49643bc8ecfa51b7a4f7ce5308
semitrivial <alexander@math.ohio-state.edu>
2019-12-15 15:28:37 -0500

    Simplify Lv5/w^w.py

--- src/Lv5/w^w.py
+++ src/Lv5/w^w.py
@@ -1,17 +1,17 @@
 X=get_file('1.py')
 
-i = 1
+i = 0
 while True:
     output(X)
+    i += 1
     Xi = "X" + str(i)
     X = """
 TEMPLATE = '''"""+escape(X)+"""'''
-TEMPLATE = TEMPLATE.replace('=\\""', '=\\\"\"\""""+Xi+"""\"\"\"')
+TEMPLATE = TEMPLATE.replace('=\\""', '=\\\"\"\"Xi\"\"\"')
 
-"""+Xi+"""=""
+Xi=""
 
 while True:
-    output("""+Xi+""")
-    """+Xi+""" = TEMPLATE.replace(\""""+Xi+"""\", escape("""+Xi+"""))
-"""
-    i += 1
+    output(Xi)
+    Xi = TEMPLATE.replace(\"Xi\", escape(Xi))
+""".replace("Xi", Xi)
