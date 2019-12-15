3312afffaa9ad19a07e1799b6859fad078e91bb7
semitrivial <alexander@math.ohio-state.edu>
2019-12-15 14:22:30 -0500

    Remove Lv6 (not ready for it yet)

--- src/Lv6/test.py
+++
@@ -1,8 +0,0 @@
-from util import *
-
-def test_level_6():
-    print("Testing w^w.py")
-    assert looks_like_omega_to_omega(ord_file("w^w"))
-
-    print("Testing w^w+1.py")
-    assert looks_like_omega_to_omega(ord_file("w^w+1")-1)
--- src/Lv6/w^w+1.py
+++
@@ -1,2 +0,0 @@
-X0=get_file("w^w.py")
-output(X0)
